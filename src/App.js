0c63d1c3f0fd6196469b13097f82a18e28783cb2
Jhabindra Aryal <learnerprak@gmail.com>
2021-05-27 16:21:15 +0545

    reactProject-02--Tour-list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React, {useState} from 'react';
-import data from './data';
-import List from './list'
-
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  const [people, setPeople] = useState(data);
-  return (
-   <main>
-     <section className="container">
-       <h3>{people.length} Birthday Today</h3>
-       <List people={people}/>
-       <button onClick={()=>setPeople([])}>Clear All</button>
-     </section>
-   </main>
-  );
+  const [loading,setLoading] = useState(true);
+  const [tours,setTours] = useState([]);
+
+const removeTour = (id)=>{
+const newTours = tours.filter((tour)=>tour.id!==id);
+setTours(newTours);
 }
 
-export default App;
+const fetchTours = async ()=>{
+  setLoading(true);
+  try{
+  const response = await fetch(url);
+  const tours = await response.json();
+  setLoading(false);
+  setTours(tours);
+  }catch(error){
+setLoading(false);
+console.log(error.message);
+  }
+};
+
+useEffect(()=>fetchTours(),[]);
+
+
+
+  if(loading){
+    return(
+      <main>
+<Loading />
+      </main>
+    );
+  }
+  if(tours.length ===0){
+    return <main>
+      <div className="title">
+       <h2>No Tours Left</h2> 
+       <button className="btn" onClick={fetchTours}>Refresh</button>
+      </div>
+    </main>
+  }
+
+  return <main>
+    <Tours  tours={tours} removeTour = {removeTour}/>
+  </main>
+}
+
+export default App
